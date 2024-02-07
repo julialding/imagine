56a9bc95daa4b11c7b52fc94caa6cf89579a0f40
Samay Lakhani <47613687+BlockchainRev@users.noreply.github.com>
2024-02-06 19:48:45 -0500

    Update image_descriptor.py

--- image_descriptor.py
+++ image_descriptor.py
@@ -1,12 +1,36 @@
 # AI Code that will run on the image to generate a description
+from openai import OpenAI
+import os
 global descriptioned
+os.environ['OPENAI_API_KEY'] = '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'
 # Microsoft code here
 def generate_description():
     print("Classifying image")
-    global descriptioneds
+    # global descriptioneds
     descriptioned = True
+    client = OpenAI()
+    response = client.chat.completions.create(
+    model="gpt-4-vision-preview",
+    messages=[
+        {
+        "role": "user",
+        "content": [
+            {"type": "text", "text": "What’s in this image?"},
+            {
+            "type": "image_url",
+            "image_url": {
+                "url": "https://upload.wikimedia.org/wikipedia/commons/thumb/d/dd/Gfp-wisconsin-madison-the-nature-boardwalk.jpg/2560px-Gfp-wisconsin-madison-the-nature-boardwalk.jpg",
+            },
+            },
+        ],
+        }
+    ],
+    max_tokens=300,
+    )
 
+    print(response.choices[0])
 
+generate_description()
 # returns if image has a description ready
 def image_decriptioned():
     return descriptioned
